5a8a9d4023dbe046687e99fe864afc1fd576ca3e
“chisaki0606” <witness6961@gmail.com>
2020-05-02 23:51:28 +0900

    add CRUD

--- board/views.py
+++ board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import TopicCreateForm
 from .models import Topic
 
@@ -17,3 +17,31 @@ def add(request):
     'form':form
   }
   return render(request, 'board/form.html', context)
+
+def update(request, pk):
+  topic = get_object_or_404(Topic, pk=pk)
+  form = TopicCreateForm(request.POST or None, instance=topic)
+  if request.method == 'POST' and form.is_valid():
+    form.save()
+    return redirect('board:index')
+  context = {
+    'form':form
+  }
+  return render(request, 'board/form.html', context)
+
+def delete(request, pk):
+  topic = get_object_or_404(Topic, pk=pk)
+  if request.method == 'POST':
+    topic.delete()
+    return redirect('board:index')
+  context = {
+    'topic':topic,
+  }
+  return render(request, 'board/confirm_delete.html', context)
+
+def detail(request, pk):
+  topic = get_object_or_404(Topic, pk=pk)
+  context = {
+    'topic':topic,
+  }
+  return render(request, 'board/detail.html', context)
